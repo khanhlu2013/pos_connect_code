799ea2b25387e60594213a1960cf85ac69ae853e
Khanh Tran <khanhlu2013@gmail.com>
2014-03-07 20:11:19 -0800

    ..

--- liquor/settings.py
+++ liquor/settings.py
@@ -8,10 +8,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+from unipath import Path
+PROJECT_ROOT = Path(__file__).ancestor(2)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -50,20 +49,6 @@ ROOT_URLCONF = 'liquor.urls'
 
 WSGI_APPLICATION = 'liquor.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -93,13 +78,14 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = ''
 STATIC_URL = '/static/' 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_resource'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    PROJECT_ROOT.child('static_resource'),
 )
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -109,7 +95,7 @@ STATICFILES_FINDERS = (
 
 
 TEMPLATE_DIRS = (
-    BASE_DIR.child('templates'),
+    PROJECT_ROOT.child('templates'),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (

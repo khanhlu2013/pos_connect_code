9f092d27c33ff3c4a578c13decffb912efb0a573
Khanh Tran <khanhlu2013@gmail.com>
2014-03-12 00:44:45 -0700

    ..

--- sale/views.py
+++ sale/views.py
@@ -24,7 +24,7 @@ class Sale_view(TemplateView):
         print('-xxx-')
         client_user_name = 'thanarzandimandstoweress'
         client_user_password = 'T7UxkEMIq1mQVER1n7gTgPSt'
-        couch_server_url = couch_util.get_url(client_user_name,client_user_password) 
+        couch_server_url = couch_util.get_url_using_admin_account()
         store_db_name = store_util.get_store_db_name(bus_id)
 
         context['store_db_name'] = store_db_name

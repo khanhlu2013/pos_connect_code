d2499eae528a40b2800235cc6c7f004212ccbbba
Khanh Tran <khanhlu2013@gmail.com>
2014-06-22 11:52:48 -0700

    ..

--- liquor/urls.py
+++ liquor/urls.py
@@ -25,7 +25,14 @@ urlpatterns = patterns('',
     url(r'^account/login/$','django.contrib.auth.views.login',{'template_name':'login.html'},name = 'liquor_login_named_url'),
     url(r'^account/logout/$','django.contrib.auth.views.logout_then_login',name = 'liquor_logout_named_url'),
     url(r'^sale/',include(sale.urls,namespace='sale')),
+    url(r'^sale_report/',include(sale_report.urls,namespace='sale_report')),
+    url(r'^sale_shortcut/',include(sale_shortcut.urls,namespace='sale_shortcut')),
+    url(r'^tax/',include(tax.urls,namespace='tax')),
     url(r'^product/',include(store_product.urls,namespace='store_product')),
+    url(r'^mix_match/',include(mix_match.urls,namespace='mix_match')),
+    url(r'^group/',include(group.urls,namespace='group')),
+    url(r'^receipt/',include(receipt.urls,namespace='receipt')),    
+    url(r'^payment_type/',include(payment_type.urls)),      
 )
 
 handler500 = curry(server_error, template_name='500.html')
--- static_resource/js/app/product/product_entry.js
+++ static_resource/js/app/product/product_entry.js
@@ -23,12 +23,11 @@ requirejs.onError = function (err) {
 //http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
 window.name = "NG_DEFER_BOOTSTRAP!";
 
-require( [
-    'angular',
+require( 
+[
     'app/product/product',
-], function(angular, app) {
-    angular.element().ready(function() {
-        angular.resumeBootstrap([app['name']]);
-    });
+]
+, function(app) {
+
 });
 

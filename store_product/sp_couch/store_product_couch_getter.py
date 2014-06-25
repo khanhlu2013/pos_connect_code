585688477fe8aae7590977476627c9ae014f5227
Khanh Tran <khanhlu2013@gmail.com>
2014-06-25 15:18:23 -0700

    ..

--- store_product/sp_couch/store_product_couch_getter.py
+++ store_product/sp_couch/store_product_couch_getter.py
@@ -18,7 +18,7 @@ def exe(product_id,store_id):
     else: return rows[0].value
 
 
-def get_lst(pid_lst,store_id):
+def get_lst_old(pid_lst,store_id):
     result = []
 
     db = couch_util.get_store_db(store_id)
@@ -29,4 +29,12 @@ def get_lst(pid_lst,store_id):
         if row.value['product_id'] in pid_lst:
             result.append(row.value)
 
+    return result
+
+def get_lst(pid_lst,store_id):
+    db = couch_util.get_store_db(store_id)
+    view_name = couch_util._get_full_view_name(couch_constance.STORE_DB_VIEW_NAME_BY_PRODUCT_ID)
+    rows = db.view(view_name,keys=pid_lst,include_docs=True)
+
+    result = [row.doc for row in rows]
     return result

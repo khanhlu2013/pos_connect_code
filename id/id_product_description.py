f82616d2a1a25381457467bac1e6a2e12ff9f4d4
Khanh Tran <khanhlu2013@gmail.com>
2014-10-27 14:38:35 -0700

    ..; git push hp

--- id/id_product_description.py
+++ id/id_product_description.py
@@ -38,3 +38,41 @@ def exe():
                     )
 
     log_file.close() # you can omit in most cases as the destructor will call if
+
+def exe_left_over():
+    store = Store.objects.get(name = "1")
+    with open('id/data_tiep_product_description_full.txt', 'rb') as csvfile:
+        master_data = {}
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for raw in spamreader:
+            # ItemNum,ItemName,Dept_ID,Description
+
+            sku = raw[0]
+            product_name = raw[1]
+            dep_id = raw[2]
+            p_type = raw[3]
+
+            master_data[sku] = p_type
+
+        sp_lst = Store_product.objects.filter(store=store,p_type=None)
+        for sp in sp_lst:
+            a_sku = sp.product.sku_set.all()[0].sku
+            p_type = master_data[a_sku]
+            if p_type != None:
+                sp_updator.exe(
+                     product_id = sp.product.id
+                    ,store_id = store.id
+                    ,name = sp.name
+                    ,price = float(sp.price) if sp.price != None else None
+                    ,value_customer_price = float(sp.value_customer_price) if sp.value_customer_price != None else None
+                    ,crv = float(sp.crv) if sp.crv != None else None
+                    ,is_taxable = sp.is_taxable
+                    ,is_sale_report  = sp.is_sale_report
+                    ,p_type = p_type
+                    ,p_tag = sp.p_tag
+                    ,vendor = sp.vendor
+                    ,cost = float(sp.cost) if sp.cost != None else None
+                    ,buydown = float(sp.buydown) if sp.buydown != None else None
+                )
+
+ 

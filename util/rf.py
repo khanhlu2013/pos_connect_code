08deeeba52b259a3d5085ad80f77e18b1c378608
Khanh Tran <khanhlu2013@gmail.com>
2014-11-01 20:26:48 -0700

    ..

--- util/rf.py
+++ util/rf.py
@@ -11,10 +11,10 @@ def d():
     print('----------- creating 4 empty stores ----------------')
     delete_data()
 
-user1,store1=_create_user_then_store_detail(user_name = "1",user_password="1",store_name="1")
-user2,store2=_create_user_then_store_detail(user_name = "2",user_password="2",store_name="2")
-user3,store3=_create_user_then_store_detail(user_name = "3",user_password="3",store_name="3") 
-user4,store4=_create_user_then_store_detail(user_name = "4",user_password="4",store_name="4") 
+    user1,store1=_create_user_then_store_detail(user_name = "1",user_password="1",store_name="1")
+    user2,store2=_create_user_then_store_detail(user_name = "2",user_password="2",store_name="2")
+    user3,store3=_create_user_then_store_detail(user_name = "3",user_password="3",store_name="3") 
+    user4,store4=_create_user_then_store_detail(user_name = "4",user_password="4",store_name="4") 
 
 def _create_user_then_store_detail(user_name,user_password,store_name,tax_rate=8.725):
     #helper: create user belong to that single store
@@ -22,7 +22,7 @@ def _create_user_then_store_detail(user_name,user_password,store_name,tax_rate=8
     user = User.objects.create_user(username = user_name,password=user_password,first_name=user_name)
     membership = mommy.make('liqUser.Membership',business=store,user=user)
     return (membership.user,membership.user.business_set.all()[0])
-
+    
 def delete_data():
     Group.objects.all().delete()
     Mix_match.objects.all().delete()    

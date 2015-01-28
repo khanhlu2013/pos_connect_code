ddf37c8b81ae84ee72d995d5fedf7c02636c7ef4
Khanh Tran <khanhlu2013@gmail.com>
2015-01-27 23:12:32 -0800

    ..

--- liquor/settings.py
+++ liquor/settings.py
@@ -41,6 +41,9 @@ STATIC_URL = os.environ.get('STATIC_URL')
 TEMPLATE_DIRS = ( PROJECT_ROOT.child('templates'), )
 ROOT_URLCONF = 'liquor.urls'
 TEMPLATE_LOADERS = ( 'django.template.loaders.filesystem.Loader', )
+LOGIN_URL = 'liquor_login_named_url'
+LOGOUT_URL = 'liquor_logout_named_url'
+LOGIN_REDIRECT_URL  = 'sp_search'
 
 #- this will read the environment variable DATABASE_URL and translate it for django to understand
 import dj_database_url

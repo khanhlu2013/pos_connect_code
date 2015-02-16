b17771e776d4c09ec52e49d67e2c7e42edffd91d
Khanh Tran <khanhlu2013@gmail.com>
2015-02-16 10:19:24 -0800

    ..

--- test/unit/model/product/network_product/template.summary.js
+++ test/unit/model/product/network_product/template.summary.js
@@ -62,7 +62,219 @@ describe('model.product.network_product.main.summary.html',function(){
         expect(summary_lst_node[member_count_index].textContent.indexOf(network_product_sp_lst.length)>-1).toEqual(true);
     });
 
-    it('has main.summary.html that can display suggestion for taxable',function(){
-        //taxable
-    })
+    it('has main.summary.html that can hide both is_taxable glyphicon and percent if tax percent is 50%',function(){
+        //init controller
+        var share_setting_mock = {}
+        createControllerAndView(share_setting_mock);      
+        var network_product = {};
+        network_product.get_suggest_extra = jasmine.createSpy();
+        var sp_1 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var sp_2 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var network_product_sp_lst = [sp_1,sp_2];
+        network_product.get_sp_lst = jasmine.createSpy().and.returnValue(network_product_sp_lst);
+        network_product.get_suggest_main = jasmine.createSpy().and.callFake(function(field){
+            if(field === 'is_taxable'){
+                return {percent:50}
+            }
+        })
+        scope.init(network_product);   
+        scope.$digest();    
+
+        //calculate index
+        var summary_lst_node = view[0].childNodes[1].childNodes[2].childNodes[2].childNodes;
+        var name_index = 1;
+        var member_count_index = 3;
+        var price_index = 5;
+        var tax_index = 7;
+        var crv_index = 9;
+        var cost_index = 11;
+
+        var tax_node_p = summary_lst_node[tax_index].childNodes[3];
+        expect(tax_node_p.className.indexOf('ng-hide')>-1).toEqual(true);
+    });
+
+    it('has main.summary.html that can show both is_taxable glyphicon and percent if tax percent is not 50%',function(){
+        //init controller
+        var share_setting_mock = {}
+        createControllerAndView(share_setting_mock);      
+        var network_product = {};
+        network_product.get_suggest_extra = jasmine.createSpy();
+        var sp_1 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var sp_2 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var network_product_sp_lst = [sp_1,sp_2];
+        network_product.get_sp_lst = jasmine.createSpy().and.returnValue(network_product_sp_lst);
+        network_product.get_suggest_main = jasmine.createSpy().and.callFake(function(field){
+            if(field === 'is_taxable'){
+                return {percent:51}
+            }
+        })
+        scope.init(network_product);   
+        scope.$digest();    
+
+        //calculate index
+        var summary_lst_node = view[0].childNodes[1].childNodes[2].childNodes[2].childNodes;
+        var name_index = 1;
+        var member_count_index = 3;
+        var price_index = 5;
+        var tax_index = 7;
+        var crv_index = 9;
+        var cost_index = 11;
+
+        var tax_node_p = summary_lst_node[tax_index].childNodes[3];
+        expect(tax_node_p.className.indexOf('ng-hide')>-1).toEqual(false);
+    });
+
+    it('has main.summary.html that can show is_taxable of true as glyphicon-check',function(){
+        //init controller
+        var share_setting_mock = {}
+        createControllerAndView(share_setting_mock);      
+        var network_product = {};
+        network_product.get_suggest_extra = jasmine.createSpy();
+        var sp_1 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var sp_2 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var network_product_sp_lst = [sp_1,sp_2];
+        network_product.get_sp_lst = jasmine.createSpy().and.returnValue(network_product_sp_lst);
+        network_product.get_suggest_main = jasmine.createSpy().and.callFake(function(field){
+            if(field === 'is_taxable'){
+                return {percent:51,value:true}
+            }
+        })
+        scope.init(network_product);   
+        scope.$digest();    
+
+        //calculate index
+        var summary_lst_node = view[0].childNodes[1].childNodes[2].childNodes[2].childNodes;
+        var name_index = 1;
+        var member_count_index = 3;
+        var price_index = 5;
+        var tax_index = 7;
+        var crv_index = 9;
+        var cost_index = 11;
+
+        var tax_node_p_glyphicon = summary_lst_node[tax_index].childNodes[3].childNodes[1];
+        expect(tax_node_p_glyphicon.className.indexOf('glyphicon-check')>-1).toEqual(true);
+    });
+
+    it('has main.summary.html that can show is_taxable of false as glyphicon-unchecked',function(){
+        //init controller
+        var share_setting_mock = {}
+        createControllerAndView(share_setting_mock);      
+        var network_product = {};
+        network_product.get_suggest_extra = jasmine.createSpy();
+        var sp_1 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var sp_2 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var network_product_sp_lst = [sp_1,sp_2];
+        network_product.get_sp_lst = jasmine.createSpy().and.returnValue(network_product_sp_lst);
+        network_product.get_suggest_main = jasmine.createSpy().and.callFake(function(field){
+            if(field === 'is_taxable'){
+                return {percent:51,value:false}
+            }
+        })
+        scope.init(network_product);   
+        scope.$digest();    
+
+        //calculate index
+        var summary_lst_node = view[0].childNodes[1].childNodes[2].childNodes[2].childNodes;
+        var name_index = 1;
+        var member_count_index = 3;
+        var price_index = 5;
+        var tax_index = 7;
+        var crv_index = 9;
+        var cost_index = 11;
+
+        var tax_node_p_glyphicon = summary_lst_node[tax_index].childNodes[3].childNodes[1];
+        expect(tax_node_p_glyphicon.className.indexOf('glyphicon-unchecked')>-1).toEqual(true);
+    });
+
+    it('has main.summary.html that can hide is_taxable percent if percent is 100',function(){
+        //init controller
+        var share_setting_mock = {}
+        createControllerAndView(share_setting_mock);      
+        var network_product = {};
+        network_product.get_suggest_extra = jasmine.createSpy();
+        var sp_1 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var sp_2 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var network_product_sp_lst = [sp_1,sp_2];
+        network_product.get_sp_lst = jasmine.createSpy().and.returnValue(network_product_sp_lst);
+        network_product.get_suggest_main = jasmine.createSpy().and.callFake(function(field){
+            if(field === 'is_taxable'){
+                return {percent:100,value:false}
+            }
+        })
+        scope.init(network_product);   
+        scope.$digest();    
+
+        //calculate index
+        var summary_lst_node = view[0].childNodes[1].childNodes[2].childNodes[2].childNodes;
+        var name_index = 1;
+        var member_count_index = 3;
+        var price_index = 5;
+        var tax_index = 7;
+        var crv_index = 9;
+        var cost_index = 11;
+
+        var tax_node_p_percent = summary_lst_node[tax_index].childNodes[3].childNodes[3];
+        expect(tax_node_p_percent.className.indexOf('ng-hide')>-1).toEqual(true);
+    });
+    it('has main.summary.html that can show is_taxable percent if percent is not 100',function(){
+        //init controller
+        var share_setting_mock = {}
+        createControllerAndView(share_setting_mock);      
+        var network_product = {};
+        network_product.get_suggest_extra = jasmine.createSpy();
+        var sp_1 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var sp_2 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var network_product_sp_lst = [sp_1,sp_2];
+        network_product.get_sp_lst = jasmine.createSpy().and.returnValue(network_product_sp_lst);
+        network_product.get_suggest_main = jasmine.createSpy().and.callFake(function(field){
+            if(field === 'is_taxable'){
+                return {percent:70,value:false}
+            }
+        })
+        scope.init(network_product);   
+        scope.$digest();    
+
+        //calculate index
+        var summary_lst_node = view[0].childNodes[1].childNodes[2].childNodes[2].childNodes;
+        var name_index = 1;
+        var member_count_index = 3;
+        var price_index = 5;
+        var tax_index = 7;
+        var crv_index = 9;
+        var cost_index = 11;
+
+        var tax_node_p_percent = summary_lst_node[tax_index].childNodes[3].childNodes[3];
+        expect(tax_node_p_percent.className.indexOf('ng-hide')>-1).toEqual(false);
+    });
+    it('has main.summary.html that can show is_taxable percent amount',function(){
+        //init controller
+        var share_setting_mock = {}
+        createControllerAndView(share_setting_mock);      
+        var network_product = {};
+        network_product.get_suggest_extra = jasmine.createSpy();
+        var sp_1 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var sp_2 = {get_cost:jasmine.createSpy()}//this way the empty_to_end filter can function
+        var network_product_sp_lst = [sp_1,sp_2];
+        network_product.get_sp_lst = jasmine.createSpy().and.returnValue(network_product_sp_lst);
+        var percent = 75;
+        network_product.get_suggest_main = jasmine.createSpy().and.callFake(function(field){
+            if(field === 'is_taxable'){
+                return {percent:percent,value:false}
+            }
+        })
+        scope.init(network_product);   
+        scope.$digest();    
+
+        //calculate index
+        var summary_lst_node = view[0].childNodes[1].childNodes[2].childNodes[2].childNodes;
+        var name_index = 1;
+        var member_count_index = 3;
+        var price_index = 5;
+        var tax_index = 7;
+        var crv_index = 9;
+        var cost_index = 11;
+
+        var tax_node_p_percent = summary_lst_node[tax_index].childNodes[3].childNodes[3];
+        expect(tax_node_p_percent.textContent.indexOf(percent+'%')>-1).toEqual(true)
+    });
 })
